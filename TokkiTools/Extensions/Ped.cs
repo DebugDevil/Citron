07f0a2b7bb50d4290e75ea380b96d920165dbd0a
Hannele Ruiz <justlemoncl@gmail.com>
2019-06-08 17:05:41 -0400

    Added function to check a ped is friendly with a player

---
+++ TokkiTools/Extensions/Ped.cs
@@ -0,0 +1,19 @@
+﻿using GTA;
+
+namespace TokkiTools.Extensions
+{
+    /// <summary>
+    /// Extension for the game ped.
+    /// </summary>
+    public static class PedExtensions
+    {
+        /// <summary>
+        /// If the ped is friendly with the player.
+        /// </summary>
+        /// <returns>true if the ped and player are companions, respect or like each other, false otherwise. </returns>
+        public static bool IsFriendly(this Ped GamePed)
+        {
+            return Game.Player.Character.GetRelationshipWithPed(GamePed) <= Relationship.Like && GamePed.GetRelationshipWithPed(Game.Player.Character) <= Relationship.Like;
+        }
+    }
+}

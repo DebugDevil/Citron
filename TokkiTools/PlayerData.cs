6f7e053de8fc940ecbf9adb515d7ae4b8074003e
Hannele Ruiz <justlemoncl@gmail.com>
2019-06-08 17:17:26 -0400

    Added functions to get the player/vehicle Position and Rotation

---
+++ TokkiTools/PlayerData.cs
@@ -0,0 +1,33 @@
+﻿using GTA;
+using GTA.Math;
+
+namespace TokkiTools
+{
+    /// <summary>
+    /// Another ways to get the main player data.
+    /// </summary>
+    public static class PlayerData
+    {
+        /// <summary>
+        /// Gets the position of the player ped or (if is present) the player vehicle.
+        /// </summary>
+        public static Vector3 Position
+        {
+            get
+            {
+                return Game.Player.Character.CurrentVehicle != null ? Game.Player.Character.CurrentVehicle.Position : Game.Player.Character.Position;
+            }
+        }
+
+        /// <summary>
+        /// Gets the rotation of the player ped or (if is present) the player vehicle.
+        /// </summary>
+        public static Vector3 Rotation
+        {
+            get
+            {
+                return Game.Player.Character.CurrentVehicle != null ? Game.Player.Character.CurrentVehicle.Rotation : Game.Player.Character.Rotation;
+            }
+        }
+    }
+}

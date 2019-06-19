870caace09924ca4799c879625e15b44370631d7
Hannele Ruiz <justlemoncl@gmail.com>
2019-06-19 05:34:35 -0400

    Added converter from Known Color Name to Color class

---
+++ ColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Drawing;
+
+namespace Citron
+{
+    /// <summary>
+    /// A Json.Net converter from string to Color and vice versa.
+    /// </summary>
+    public class ColorConverter : JsonConverter<Color>
+    {
+        public override Color ReadJson(JsonReader reader, Type objectType, Color existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            // Return the value from the known color
+            return Color.FromKnownColor((KnownColor)Enum.Parse(typeof(KnownColor), (string)reader.Value));
+        }
+
+        public override void WriteJson(JsonWriter writer, Color value, JsonSerializer serializer)
+        {
+            // We don't need to write anything
+            throw new NotImplementedException();
+        }
+    }
+}

39f35c0cc1aede77b21b36aec446ef0c2ae237ef
Hannele Ruiz <justlemoncl@gmail.com>
2019-06-20 05:10:57 -0400

    Added Color extension that sets the transparency to 50

---
+++ Extensions/Color.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace Citron.Extensions
+{
+    public static class ColorExtensions
+    {
+        /// <summary>
+        /// Sets the transparency of the color with a value of 50.
+        /// </summary>
+        public static Color Clear(this Color color)
+        {
+            return Color.FromArgb(50, color.R, color.G, color.B);
+        }
+    }
+}

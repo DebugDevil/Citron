b8b3b5b0a8b58c58b491da87e29d6752467292b3
Hannele Ruiz <justlemoncl@gmail.com>
2019-06-30 17:30:01 -0400

    Added functions to get and set the current and max health
    The current implementation of SHVDN is flawed

---
+++ Extensions/Entity.cs
@@ -0,0 +1,28 @@
+﻿using GTA;
+using GTA.Native;
+
+namespace Citron.Extensions
+{
+    public static class EntityExtensions
+    {
+        public static int GetMaxHealth(this Entity GamePed)
+        {
+            return Function.Call<int>(Hash.GET_ENTITY_MAX_HEALTH, GamePed);
+        }
+
+        public static int GetHealth(this Entity GamePed)
+        {
+            return Function.Call<int>(Hash.GET_ENTITY_HEALTH, GamePed);
+        }
+
+        public static int SetMaxHealth(this Entity GamePed)
+        {
+            return Function.Call<int>(Hash.SET_ENTITY_MAX_HEALTH, GamePed);
+        }
+
+        public static int SetHealth(this Entity GamePed)
+        {
+            return Function.Call<int>(Hash.SET_ENTITY_HEALTH, GamePed);
+        }
+    }
+}
